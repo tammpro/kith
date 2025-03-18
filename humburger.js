e29c042564e044b0405bbeac72585493a6e9dcef
tammpro <tammpro0601@gmail.com>
2025-03-18 11:27:33 +0900

    Delete humburger.js

--- humburger.js
+++
@@ -1,9 +0,0 @@
-"use strict"
-
-const humburger = document.querySelector(".humburger");
-const box = document.querySelector(".box");
-
-humburger.addEventListener("click",()=>{
-    humburger.classList.toggle("open");
-    box.classList.toggle("open");
-});
